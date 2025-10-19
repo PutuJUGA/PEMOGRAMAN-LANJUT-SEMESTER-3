90109d54670979e6d8a7bc6a4e9382ed2aa77754
PutuJUGA <givrantjanuarta@gmail.com>
2025-10-20 03:01:37 +0700

    CodeLab 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //Code Sederhana untuk versi pertama
+        System.out.println("Aplikasi dimulai.");
+        System.out.println("Selamat datang di proyek pengendalian versi!");
+    }
+}
